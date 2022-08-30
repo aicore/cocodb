6f1943de9d7ae3797cb35e4a9ca5abcb16a6f93e
Charly Abraham <charly@core.ai>
2022-08-30 22:31:36 +0530

    feat: inital code to poc fastify and json verification

---
+++ src/api/put.js
@@ -0,0 +1,47 @@
+import {put} from "@aicore/libmysql";
+
+// Refer https://json-schema.org/understanding-json-schema/index.html
+const putSchema = {
+    schema: {
+        body: {
+            type: 'object',
+            required: ['tableName', 'document'],
+            properties: {
+                tableName: {
+                    type: 'string',
+                    minLength: 1,
+                    maxLength: 64
+                },
+                document: {
+                    type: 'object',
+                    minProperties: 1
+                }
+            }
+        },
+        response: {
+            200: {
+                type: 'object',
+                required: ['isSuccess'],
+                properties: {
+                    documentId: {type: 'string'},
+                    isSuccess: {type: 'boolean'},
+                    errorMessage: {type: 'string'}
+                }
+            }
+        }
+    }
+};
+
+export function getPutSchema() {
+    return putSchema;
+}
+
+export async function putDocument(request, reply) {
+    const tableName = request.body.tableName;
+    const document = request.body.document;
+    try {
+        const documentId = await put(tableName, document);
+    } catch (e) {
+
+    }
+}
