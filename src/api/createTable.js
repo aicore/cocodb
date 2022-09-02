b64469f06ee5ecfca50f4a98ba69fee20410c0c4
Charly Abraham <charly@core.ai>
2022-09-02 13:26:48 +0530

    tests: ut for createTable

--- src/api/createTable.js
+++ src/api/createTable.js
@@ -1,20 +1,20 @@
-import {createTable as mysqlCreateTable} from "@aicore/libmysql";
+import LibMySql from "@aicore/libmysql";
 
-async function _createTable(tableName) {
+async function _createTable(request, reply, tableName) {
     const response = {
         isSuccess: false
     };
     try {
-        console.log(`tableName = ${tableName}`);
-        const isSuccess = await mysqlCreateTable(tableName);
+        request.log.info(`tableName = ${tableName}`);
+        const isSuccess = await LibMySql.createTable(tableName);
         response.isSuccess = isSuccess;
 
     } catch (e) {
-        response.errorMessage = `Exception occurred while creating table \n ${e}`;
-        console.error(e);
+        reply.code(400);
+        response.errorMessage = `Exception occurred while creating table`;
+        request.log.error(e);
     }
     return response;
-
 }
 
 // Refer https://json-schema.org/understanding-json-schema/index.html
@@ -39,6 +39,14 @@ const creatTableSchema = {
                     isSuccess: {type: 'boolean'},
                     errorMessage: {type: 'string'}
                 }
+            },
+            400: {
+                type: 'object',
+                required: ['isSuccess', 'errorMessage'],
+                properties: {
+                    isSuccess: {type: 'boolean'},
+                    errorMessage: {type: 'string'}
+                }
             }
         }
     }
@@ -49,7 +57,11 @@ export function getCreatTableSchema() {
 }
 
 export async function createTable(request, reply) {
+    request.log.error('hello');
     const tableName = request.body.tableName;
-    const response = await _createTable(tableName);
+    const response = await _createTable(request, reply, tableName);
+    if (!response.isSuccess) {
+        reply.code(400);
+    }
     return response;
 }
