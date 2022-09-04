0a9e7db51e3faae59d4006fc183895d52fac4e40
Charly Abraham <charly@core.ai>
2022-09-04 11:15:12 +0530

    feat: added deleteDocumet API

---
+++ src/api/deleteKey.js
@@ -0,0 +1,52 @@
+import LibMySql from "@aicore/libmysql";
+import {HTTP_STATUS_CODES} from "@aicore/libcommonutils";
+
+const BAD_REQUEST = HTTP_STATUS_CODES.BAD_REQUEST;
+
+// Refer https://json-schema.org/understanding-json-schema/index.html
+const deleteKeySchema = {
+    schema: {
+        body: {
+            type: 'object', required: ['tableName', 'documentId'], properties: {
+                tableName: {
+                    type: 'string', minLength: 1, maxLength: 64
+                }, documentId: {
+                    type: 'string', minLength: 1, maxLength: 32
+                }
+            }
+        }, response: {
+            200: { //HTTP_STATUS_CODES.OK
+                type: 'object', required: ['isSuccess'], properties: {
+                    isSuccess: {type: 'boolean'}, errorMessage: {type: 'string'}
+                }
+            }, 400: { //HTTP_STATUS_CODES.BAD_REQUEST
+                type: 'object', required: ['errorMessage'], properties: {
+                    errorMessage: {type: 'string'}
+                }
+            }
+        }
+    }
+};
+
+export function getDeleteKeySchema() {
+    return deleteKeySchema;
+}
+
+export async function deleteKey(request, reply) {
+    const tableName = request.body.tableName;
+    const documentId = request.body.documentId;
+
+    const response = {
+        isSuccess: false
+    };
+    try {
+        request.log.info(`tableName = ${tableName} documentID =${documentId}`);
+        response.isSuccess = await LibMySql.deleteKey(tableName, documentId);
+
+    } catch (e) {
+        reply.code(BAD_REQUEST);
+        response.errorMessage = e.toString();
+        request.log.error(e);
+    }
+    return response;
+}
