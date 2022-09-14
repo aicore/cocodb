7e5a87cf74e01ec4bb7dc9f2b3dfd2264d77f574
Charly Abraham <charly@core.ai>
2022-09-14 17:54:53 +0530

    test: integ test for 1500 put get delete

--- test/integration/api-test.spec.js
+++ test/integration/api-test.spec.js
@@ -25,10 +25,10 @@ import {
     deleteDocument,
     deleteTable,
     get,
-    getFromIndex,
+    getFromIndex, getFromNonIndex,
     hello,
     init,
-    put
+    put, update
 } from "@aicore/coco-db-client";
 
 let expect = chai.expect;
@@ -146,5 +146,142 @@ describe('Integration: Hello world Tests', function () {
             expect(newPutResp.isSuccess).eql(false);
             expect(newPutResp.errorMessage).eql("Error: Duplicate entry '12345' for key 'customers.b80bb7740288fda1f201890375a60c8f'");
         });
+        it('getFromNonIndex test should pass', async function () {
+            const document = {
+                id: '12345',
+                'lastName': 'Alice',
+                'Age': 100,
+                'active': true,
+                'location': {
+                    'city': 'Banglore',
+                    'state': 'Karnataka',
+                    'layout': {
+                        'block': '1stblock'
+                    }
+
+                }
+            };
+            await put(TABLE_NAME, document);
+            await put(TABLE_NAME, document);
+            const scanResults = await getFromNonIndex(TABLE_NAME, {
+                location: {
+                    layout: {
+                        block: '1stblock'
+                    }
+                }
+            });
+            expect(scanResults.isSuccess).eql(true);
+            expect(scanResults.documents.length).eql(2);
+            expect(scanResults.documents[0].id).eql('12345');
+            expect(scanResults.documents[0].lastName).eql('Alice');
+            expect(scanResults.documents[0].Age).eql(100);
+            expect(scanResults.documents[0].location.city).eql('Banglore');
+            expect(scanResults.documents[0].location.state).eql('Karnataka');
+            expect(scanResults.documents[0].location.layout.block).eql('1stblock');
+            expect(scanResults.documents[1].id).eql('12345');
+            expect(scanResults.documents[1].lastName).eql('Alice');
+            expect(scanResults.documents[1].Age).eql(100);
+            expect(scanResults.documents[1].location.city).eql('Banglore');
+            expect(scanResults.documents[1].location.state).eql('Karnataka');
+            expect(scanResults.documents[1].location.layout.block).eql('1stblock');
+
+            const emptyResults = await getFromNonIndex(TABLE_NAME, {
+                location: {
+                    layout: {
+                        block: '2ndblock'
+                    }
+                }
+            });
+            expect(emptyResults.isSuccess).eql(true);
+            expect(emptyResults.documents.length).eql(0);
+        });
+
+        it('update document should pass', async function () {
+            const document = {
+                id: '12345',
+                'lastName': 'Alice',
+                'Age': 100,
+                'active': true,
+                'location': {
+                    'city': 'Banglore',
+                    'state': 'Karnataka',
+                    'layout': {
+                        'block': '1stblock'
+                    }
+
+                }
+            };
+            const putResp = await put(TABLE_NAME, document);
+            document.lastName = 'bob';
+            const updateResponse = await update(TABLE_NAME, putResp.documentId, document);
+            expect(updateResponse.isSuccess).eql(true);
+            const getResponse = await get(TABLE_NAME, putResp.documentId);
+            expect(getResponse.isSuccess).eql(true);
+            expect(getResponse.document.lastName).eql('bob');
+            expect(getResponse.document.Age).eql(100);
+            expect(getResponse.document.location.city).eql('Banglore');
+            expect(getResponse.document.location.state).eql('Karnataka');
+            expect(getResponse.document.location.layout.block).eql('1stblock');
+
+        });
+        it('make 1500 writes followed by read and delete', async function () {
+            await writeAndReadFromDb(1500);
+        });
+
     });
 });
+
+
+async function writeAndReadFromDb(numberOfTimes) {
+    const document = {
+        id: '12345',
+        'lastName': 'Alice',
+        'Age': 100,
+        'active': true,
+        'location': {
+            'city': 'Banglore',
+            'state': 'Karnataka',
+            'layout': {
+                'block': '1stblock'
+            }
+
+        }
+    };
+    const putPromises = [];
+    for (let i = 0; i < numberOfTimes; i++) {
+        let putResp = put(TABLE_NAME, document);
+        putPromises.push(putResp);
+    }
+    const putResults = await Promise.all(putPromises);
+    putResults.forEach(result => {
+        expect(result.isSuccess).eql(true);
+    });
+    const getPromises = [];
+    for (const resp of putResults) {
+        let getResponse = get(TABLE_NAME, resp.documentId);
+        getPromises.push(getResponse);
+    }
+    const results = await Promise.all(getPromises);
+    expect(results.length).eql(numberOfTimes);
+    results.forEach(result => {
+        expect(result.isSuccess).eql(true);
+        expect(result.document.lastName).eql('Alice');
+        expect(result.document.Age).eql(100);
+        expect(result.document.location.city).eql('Banglore');
+        expect(result.document.location.state).eql('Karnataka');
+        expect(result.document.location.layout.block).eql('1stblock');
+    });
+
+    const deletePromises = [];
+    putResults.forEach(result => {
+        let promise = deleteDocument(TABLE_NAME, result.documentId);
+        deletePromises.push(promise);
+
+    });
+
+    const deleteResults = await Promise.all(deletePromises);
+
+    deleteResults.forEach(result => {
+        expect(result.isSuccess).eql(true);
+    });
+}
