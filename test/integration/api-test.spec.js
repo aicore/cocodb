92154afef19e6781e64b9f45f2697c86fd4cb645
Charly Abraham <charly@core.ai>
2022-09-14 11:52:48 +0530

    test: integ test for put get and delete document

--- test/integration/api-test.spec.js
+++ test/integration/api-test.spec.js
@@ -12,30 +12,38 @@
 
 // remove integration tests if you don't have them.
 // jshint ignore: start
-/*global describe, it, before, after*/
+/*global describe, it, before, after, beforeEach*/
 
 import * as assert from 'assert';
 import * as chai from 'chai';
 import {getConfigs} from "./setupIntegTest.js";
 import fs from "fs";
 import {close, startDB} from "../../src/server.js";
-import {createTable, deleteTable, hello, init} from "@aicore/coco-db-client";
+import {createTable, deleteDocument, deleteTable, get, hello, init, put} from "@aicore/coco-db-client";
 
 let expect = chai.expect;
-
+const CONFIG_FILE = process.cwd() + '/conf.json';
+const TABLE_NAME = 'customers';
 describe('Integration: Hello world Tests', function () {
     before(async function () {
         const configs = await getConfigs();
-        const configFile = process.cwd() + '/conf.json';
-        fs.appendFileSync(configFile, JSON.stringify(configs));
-        process.env.APP_CONFIG = configFile;
+
+        fs.appendFileSync(CONFIG_FILE, JSON.stringify(configs));
+        process.env.APP_CONFIG = CONFIG_FILE;
         startDB();
         console.log('starting integ tests');
         init(`http://localhost:${configs.port}`, configs.authKey);
 
     });
     after(function () {
+        fs.unlinkSync(CONFIG_FILE);
         close();
+    });
+    beforeEach(async function () {
+        await createTable(TABLE_NAME);
+    });
+    afterEach(async function () {
+        await deleteTable(TABLE_NAME);
     });
 
 
@@ -44,11 +52,33 @@ describe('Integration: Hello world Tests', function () {
             const response = await hello();
             expect(response.hello).eql('world');
         });
-        it('create table should pass', async function () {
-            let resp = await createTable('customers1');
-            expect(resp.isSuccess).eql(true);
-            resp = await deleteTable('customers1');
-            expect(resp.isSuccess).eql(true);
+        it('put and get and delete to table should pass', async function () {
+            const document = {
+                'lastName': 'Alice',
+                'Age': 100,
+                'active': true,
+                'location': {
+                    'city': 'Banglore',
+                    'state': 'Karnataka',
+                    'layout': {
+                        'block': '1stblock'
+                    }
+
+                }
+            };
+            const putResp = await put(TABLE_NAME, document);
+            expect(putResp.isSuccess).eql(true);
+            expect(putResp.documentId.length).gt(10);
+            const getResp = await get(TABLE_NAME, putResp.documentId);
+            expect(getResp.isSuccess).eql(true);
+            expect(getResp.document.lastName).eql('Alice');
+            expect(getResp.document.Age).eql(100);
+            expect(getResp.document.location.city).eql('Banglore');
+            expect(getResp.document.location.state).eql('Karnataka');
+            expect(getResp.document.location.layout.block).eql('1stblock');
+            const delResp = await deleteDocument(TABLE_NAME, putResp.documentId);
+            expect(delResp.isSuccess).eql(true);
+
         });
     });
 });
