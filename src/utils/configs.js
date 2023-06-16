2787d001165bcaf678ca490e9f1dc595ed73a73a
Charly Abraham <charly@core.ai>
2023-06-16 19:22:54 +0530

    fix: config file not working with default app config

--- src/utils/configs.js
+++ src/utils/configs.js
@@ -33,7 +33,7 @@ export function getConfigs() {
         throw new Error('Please provide valid app config file by setting APP_CONFIG environment variable or' +
             ' providing valid app.json in src folder' );
     }
-    APP_CONFIG = _getAppConfig(process.env.APP_CONFIG);
+    APP_CONFIG = _getAppConfig(appConfig);
     APP_CONFIG.port = APP_CONFIG.port || '5000';
     return APP_CONFIG;
 }
